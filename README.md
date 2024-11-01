ca26b50a996e18945409ac62b3a848a894f70d84
Daniel Lawrence <brokenalarms@gmail.com>
2024-10-31 18:28:28 -0700

    first commti
    first commti
    
    bugged
    
    bugged
    
    fix trailing commas only
    
    get live debugging working
    
    get live debugging working

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vscode-sieve-formatter
-Just want trailing commas removed to not go crazy, thanks.
+
+Please note that this an incomplete formatter implementation - I just want trailing commas in lists [] and function args () removed to not go crazy while creating rules for Proton Mail! And was very suprised that no formatting extensions yet existed for Sieve.
+
+When I next have time I will try to improve this and make any 2+ item list or function arguments appear on multiple lines, to play nicer with git.
---
+++ src/extension.ts
@@ -0,0 +1,39 @@
+import * as vscode from 'vscode';
+
+
+function removeTrailingCommas(text: string): string {
+  // Match a comma followed by any whitespace or newlines, then a closing bracket
+  const listRegex = /,([\s\r\n]*(\)|\]))/g;
+  return text.replace(listRegex, '$1');
+}
+
+// Deactivate the extension
+export function deactivate() { }
+
+export function activate(context: vscode.ExtensionContext) {
+
+  // Register a formatting provider for Sieve files
+  context.subscriptions.push(
+    vscode.languages.registerDocumentFormattingEditProvider('sieve', {
+      provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+        const edits: vscode.TextEdit[] = [];
+
+        // Example: Format the entire document by replacing content
+        const fullText = document.getText();
+        const formattedText = removeTrailingCommas(fullText);
+
+        edits.push(
+          vscode.TextEdit.replace(
+            new vscode.Range(
+              document.positionAt(0),
+              document.positionAt(fullText.length)
+            ),
+            formattedText
+          )
+        );
+
+        return edits;
+      }
+    })
+  );
+}
